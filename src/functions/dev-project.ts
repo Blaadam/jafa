672992b35b3239aa4341e452342c93d731ac4866
Adam <37848102+Blaadam@users.noreply.github.com>
2026-07-11 13:32:17 +0100

    fix directory for devproject

--- src/functions/dev-project.ts
+++ src/functions/dev-project.ts
@@ -1,10 +1,27 @@
+import { log } from "../helpers/log.js";
+import { BuildProject } from "./build-project.js";
 import { OpenProject } from "./open-project.js";
 import { ServeProject } from "./serve-project.js";
+import shelly from "shelljs";
 
 export async function DevProject(
-    projectName: string,
-    target: string,
+	projectName: string,
+	target: string,
 ): Promise<string> {
-    await OpenProject(projectName, target);
-    return ServeProject(projectName, target);
+	const dir = process.cwd();
+	log.info(`Starting development for project: ${projectName}`);
+
+	shelly.cd(dir);
+	log.trace(`cwd: ${process.cwd()}, target: ${target}`);
+	log.verbose(`Building project`);
+	await BuildProject(projectName, target);
+
+	shelly.cd(dir);
+	log.trace(`cwd: ${process.cwd()}, target: ${target}`);
+	log.verbose(`Opening project`);
+	await OpenProject(projectName, target);
+
+	shelly.cd(dir);
+	log.trace(`cwd: ${process.cwd()}, target: ${target}`);
+	return ServeProject(projectName, target);
 }
