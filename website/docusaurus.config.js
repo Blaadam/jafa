3ce214c3b5eb8278f68e4a7d6427afcf63e11d4a
Adam <37848102+Blaadam@users.noreply.github.com>
2026-07-11 14:31:48 +0100

    reset baseurl

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
 	url: "https://jafa.adwo.dev",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/jafa/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
